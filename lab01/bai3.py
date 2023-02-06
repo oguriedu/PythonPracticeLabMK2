90219c12274a9b2911152ab68ccb1b670b4c3b4d
Khoa hoc du lieu 16a2 <ktphuong.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-06 20:32:07 +0700

    bai2

---
+++ lab01/bai3.py
@@ -0,0 +1,5 @@
+r = float(input('Nhập bán kính : '))
+h = float(input('Nhập chiều cao : '))
+pi = 3.14
+print('Diện tích xung quanh của hình trụ là: ',2*pi*r*h)
+print('Diện tích toàn phần của hình trụ là: ',2*pi*r*r+2*pi*r*h)

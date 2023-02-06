2cce04c47c8dfaa010bb851dbb9695747cee965a
Khoa hoc du lieu 16A1 <ntthuyen.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-06 14:06:45 +0700

    baitap

---
+++ lab01/BT TH/b1.py
@@ -0,0 +1,10 @@
+ma=int(input('Nhập mã số sinh viên: '))
+ho_ten=input('Nhập họ tên: ')
+que=input('Nhập quê quán: ')
+nam_sinh=int(input('Nhập năm sinh: '))
+diem=float(input('Nhập điểm trung bình 3 môn: '))
+print('{:<20}{:<20}{:<20}{:<12}{:<30}'.format('Mã sinh viên','Họ tên','Quê quán','Năm sinh','Điểm trung bình 3 môn'))
+print('{:<20}{:<20}{:<20}{:<12}{:<30}'.format(ma,ho_ten,que,nam_sinh,diem))
+
+
+    
---
+++ lab01/BT TH/b10.py
@@ -0,0 +1,4 @@
+import math
+x=float(input('x= '))
+f=math.log(x,4)+math.log(2,x)
+print('giá trị biểu thức= %0.2f'%f)

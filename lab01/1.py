7522198a4780fe4005664c198f176ccb6c1931a3
Huấn <tdhuan.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-06 17:11:04 +0700

    lab1

---
+++ lab01/1.py
@@ -0,0 +1,12 @@
+import os
+ma=input("ma sv:")
+ten=input("ho ten:")
+que=input("que quan:")
+nam=input("nam sinh:")
+diem=input("diem tb:")
+os.system("cls")
+print("ma sinh vien:",ma)
+print("ho ten:",ten)
+print("que quan:",que)
+print("nam sinh:",nam)
+print("diem trung binh:",diem)

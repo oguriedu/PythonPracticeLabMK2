27cfb3db57eec8aad7d23e4dc2b50d7a75843e60
Pham Thi Ngoc Tu <ptntu.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-06 23:54:09 +0700

    thuc hanh lab01 da hoan thanh bai 05

---
+++ lab01/bai5.py
@@ -0,0 +1,11 @@
+#chương trình nhập vào hai vecto a,b và tính tích vô hướng của hai vecto đó
+#vecto a:
+a1=int(input("nhập hoành độ của vecto a = "))
+a2=int(input("nhập tung độ của vecto a = "))
+print(" vecto a là : (",a1,",",a2,")")
+#vecto b:
+b1=int(input("nhập hoành độ của vecto b = "))
+b2=int(input("nhập tung độ của vecto b = "))
+print("vecto b là : (",b1,",",b2,")")
+tich_vo_huong=(a1*b1)+(a2*b2)
+print("tích vô hướng của hai vecto a , b = ",tich_vo_huong)

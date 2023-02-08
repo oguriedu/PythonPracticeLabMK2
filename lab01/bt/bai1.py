d64a3fd67cd9565a569ac4b67506f6a797f44af6
vuduchieu0002 <117256523+vuduchieu0002@users.noreply.github.com>
2023-02-08 07:09:36 -0800

    sửa lại bài 1

---
+++ lab01/bt/bai1.py
@@ -0,0 +1,9 @@
+
+
+ma_sv=int(input("Nhập mã sinh viên: "))
+hvt_sv=input("Nhập tên sinh viên: ")
+que_quan=input("Nhập quê quán: ")
+nam_sinh=input("Nhập năm sinh của sinh viên: ")
+diem_tb=float(input("Nhập điểm trung bình: "))
+print("Mã sinh viên :",ma_sv,";", "Họ và tên sinh viên :",hvt_sv,";","Quê quán :",que_quan,";","năm sinh :",nam_sinh,";","điểm trung bình :",diem_tb)        
+    

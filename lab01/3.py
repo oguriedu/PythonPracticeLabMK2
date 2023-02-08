bb6981985c6339b70c3b2a0ad71125ea74b78da9
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-08 13:05:35 +0700

    fix tên hàm bài 3

--- lab01/3.py
+++ lab01/3.py
@@ -2,10 +2,10 @@ pi=3.14
 r = float(input("Nhập bán kính của hình trụ: "))
 h = float(input("Nhập chiều cao của hình trụ: "))
 
-Sxq = 2 * pi * r * h
-Stp = 2 * pi * r**2 + Sxq
+S_xq = 2 * pi * r * h
+S_tp = 2 * pi * r**2 + S_xq
 V = (pi * r**2 * h) / 3
 
-print(f"Diện tích xung quanh của hình trụ là: {Sxq:.2f}")
-print(f"Diện tích toàn phần của hình trụ là: {Stp:.2f}")
+print(f"Diện tích xung quanh của hình trụ là: {S_xq:.2f}")
+print(f"Diện tích toàn phần của hình trụ là: {S_tp:.2f}")
 print(f"Thể tích của hình trụ là: {V:.2f}")

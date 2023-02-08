a11caa3dd5da9d1ee14e3d6498ff911b1278dd7b
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-08 12:53:37 +0700

    fix bài 11

--- lab01/11.py
+++ lab01/11.py
@@ -1,3 +1,3 @@
 n=int(input('Nhập số lần tung xúc sắc: '))
-xacsuat = 1 - (215/216)**n
-print('xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 là',round(xacsuat*100,2))
+xacsuat = (1/(6**n))**3
+print('xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 là',round(xacsuat*100,2),"%")

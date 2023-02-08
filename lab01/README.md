3263ff4cd980d39bc3035b50bb7baeb365265e96
trongviet0095 <ntviet.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-08 23:24:49 +0700

    bai lam

--- lab01/README.md
+++ lab01/README.md
@@ -1,2 +1,3 @@
 - Bài tập luyện tập lab 1
-** DHKL16A1HN - Nguyễn Trọng Việt - 22174600095 **
+**DHKL16A1HN-Nguyễn Trọng Việt-22174600095**
+
---
+++ lab01/cau1.py
@@ -0,0 +1,8 @@
+
+msv=int(input('Nhập mã số sinh viên :'))
+ho_ten=str(input('Nhập họ tên :'))
+que_quan=str(input('Nhâp quê quán :'))
+nam_sinh=int(input('Nhập năm sinh :'))
+dtb=float(input('Nhập điểm trung bình :'))
+print('{:20}{:20}{:40}{:20}{:20}'.format('Mã sinh viên','Họ tên','Quê quán','Năm sinh','Điểm TB'))
+print('{:20}{:20}{:40}{:20}{:20}'.format(msv,ho_ten,que_quan,nam_sinh,dtb))

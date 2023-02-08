92e424c0355632f1fe7750381ea9be9ec9e0b523
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-08 13:05:23 +0700

    fix bài 7

--- lab01/7.py
+++ lab01/7.py
@@ -2,13 +2,5 @@ import math
 a = float(input("Nhập a: "))
 b = float(input("Nhập b: "))
 c = float(input("Nhập c: "))
-delta = b**2 - 4*a*c
-if delta < 0:
-    print("Phương trình vô nghiệm")
-elif delta == 0:
-    x = -b / (2*a)
-    print("Phương trình có 1 nghiệm kép x = ", round(x, 2))
-else:
-    x1 = (-b + math.sqrt(delta)) / (2*a)
-    x2 = (-b - math.sqrt(delta)) / (2*a)
-    print("Phương trình có 2 nghiệm x1 =", round(x1, 2), "và x2 =", round(x2, 2))
+d = (-b/2*a)
+print('Đỉnh của phương trình bậc 2 là',d)

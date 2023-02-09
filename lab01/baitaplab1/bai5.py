7b3766676f064b5e97afa81425fca0971a716fa8
nguyenngocbacdhkl16a2hn0045 <117519519+nguyenngocbacdhkl16a2hn0045@users.noreply.github.com>
2023-02-09 20:12:02 +0700

    update

---
+++ lab01/baitaplab1/bai5.py
@@ -0,0 +1,6 @@
+a,b,c=map(float,input("nhập vào vector a:").split())
+x,y,z=map(float,input("nhập vào vector b: ").split())
+d1=a*x
+d2=b*y
+d3=c*z
+print("tích vô hướng của 2 vector là: ",d1+d2+d3)

576dc6ce4ffad8445235bfda054e239fee7686fa
Thanh16A1_0033 <117791779+NVThanh226@users.noreply.github.com>
2023-02-09 22:36:12 +0700

    Add files via upload

---
+++ Lab1/Bai07.py
@@ -0,0 +1,7 @@
+# Tìm đỉnh của phương trình bậc 2 ax^2+bx+c
+a=float(input('Nhập a = '))
+b=float(input('Nhập b = '))
+c=float(input('Nhập c = '))
+x=-b/2*a
+y=a*x**2+b*x+c
+print("Đỉnh của phương trình là : (",x,",",y,")")

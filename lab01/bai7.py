3cf24d0c35e6b319f2813e69a4c39f8e37f446ee
dohien172004 <118988397+dohien172004@users.noreply.github.com>
2023-02-09 23:00:08 +0700

    Add files via upload

---
+++ lab01/bai7.py
@@ -0,0 +1,12 @@
+#chương trình tìm đỉnh của một phương trình bậc hai
+import math
+a=float(input("nhập hệ số a = "))
+b=float(input("nhập hệ sô b = "))
+c=float(input("nhập hệ số c = "))
+if a==0 :
+    if b==0 or b!=0:
+        print("phương trình không có đỉnh")
+else:
+    delta=b**2-4*a*c
+    if delta>=0:
+        print("phương trình có đỉnh là :" ,-b/2*a)

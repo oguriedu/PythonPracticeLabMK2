3007534325e670d997eccc5fc9d01eea57bfb68d
Le Quang Linh <lqlinh.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-09 14:28:22 +0700

    thuc hanh lab01

--- lab01/Bai09.py
+++
@@ -1,6 +0,0 @@
-x = int(input('Nhập x: '))
-y = int(input('Nhập y: '))
-z = int(input('Nhập z: '))
-print("Tọa độ của điểm đối xứng với nó qua oxy là ({},{},{})".format(x,y,-z))
-print("Tọa độ của điểm đối xứng với nó qua oxz là ({},{},{})".format(x,-y,z))
-print("Tọa độ của điểm đối xứng với nó qua oyz là ({},{},{})".format(-x,y,z))
--- lab01/Bai10.py
+++
@@ -1,4 +0,0 @@
-import math
-x=float(input('Nhập x = '))
-fx=math.log(x,4)+math.log(2,x)
-print("f(x) = ",round(fx,2))

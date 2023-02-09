97206f837f91bba76a26ff8aa4e93a40bd73e1b0
NGUYEN MANH TIEN <nmtien.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-09 23:37:15 +0700

    thuc hanh lab02 da hoan thanh bai

---
+++ lab02/bai1.py
@@ -0,0 +1,10 @@
+n=int(input("Nhập số tháng: "))
+if n==1 or n==3 or n==5 or n==7 or n==8 or n==10 or n==12:
+    print("Tháng",n,"có 31 ngày trong ")
+elif n==2:
+    print("Tháng",n,"có 28 nếu năm không nhuận và có 29 ngày nếu đó là năm nhuận")
+elif n==4 or n==6 or n==9 or n==11:
+    print("Tháng",n,"có 30 ngày")
+elif n>12:
+    os.system('cls') 
+    input("Vui lòng nhập lại số tháng ")

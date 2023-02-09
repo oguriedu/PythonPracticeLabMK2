e24b9cc30d4c8b0eb248ff1b44be3f1065343266
Phùng Thị Linh-DHKL16A1HN <ptlinh.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 16:07:12 +0700

    Sua Bai11

--- lab01/Bai11.py
+++ lab01/Bai11.py
@@ -1,4 +1,4 @@
 n=int(input('Nhập số lần tung xúc xắc: '))
 #Xác suất tung n lần 1 con xúc xắc có ít nhất 1 lần ra 6
 p=1-(5/6)**n
-print("Xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 : ",round(p**n,2))
+print("Xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 : ",round(p**3,2))

0fd29c6bef86862d784052b4bb658d04e3315bd6
Huynh Ngoc Tuong Vi <hntvi.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 22:19:03 +0700

    thuc thanh lab1 da hoan thanh bai1/bai2/bai3/bai5/bai6/bai10

---
+++ lab01/3.py
@@ -0,0 +1,10 @@
+import math
+pi=3.14
+r=float(input(" nhập bán kính đáy :"))
+h= float(input(" nhập chiều cao khối trụ :"))
+Sxq= 2*pi*r*h
+Stp= 2*pi*r*h + 2*pi*r**2
+V= h*pi*r**2
+print (" diện tích xung quanh của khối trụ = : %0.2f"%Sxq)
+print (" diện tích toàn phần của khối trụ = : %0.2f"%Stp)
+print (" thể tích của khốii trụ = : %0.2f"%V)

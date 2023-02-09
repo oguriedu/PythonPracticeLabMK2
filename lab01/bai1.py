203942afafd6c72fee87322a2e4d0bc0448dfe61
NGUYEN MANH TIEN <nmtien.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-09 22:57:44 +0700

    thuc hanh lab01 da hoan thanh bai01/bai02/bai03

---
+++ lab01/bai1.py
@@ -0,0 +1,7 @@
+ma_sv=input('Ma sinh viên: ')
+ho_ten=input("Họ và tên: ")
+que_quan=input('Quê quán: ')
+nam_sinh=int(input("Năm sinh: "))
+diem=int(input("Điêm trung bình: "))
+print("Mã Sinh Viên: ",ma_sv,";","Họ và Tên: ",ho_ten,";","Quê Quán: ",que_quan,\
+     ";","Năm sinh: ",nam_sinh,";","Điểm trung bình: ",diem )
---
+++ lab01/bai10.py
@@ -0,0 +1,4 @@
+import math
+x=float(input("Nhập x: "))
+p=math.log(x,4)+math.log(2,x)
+print(round(p,2))

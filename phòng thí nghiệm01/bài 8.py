3d434439b71a719779d2103deb688dc28c6c5ca3
le thi phuong linh 16a1/0057.uneti.hn <ltplinh.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 11:30:09 +0700

    Update and rename lab01/bài 8.py to phòng thí nghiệm01/bài 8.py

---
+++ phòng thí nghiệm01/bài 8.py
@@ -0,0 +1,7 @@
+#8
+a1,a2=map(float,input("tọa độ A:").split())
+b1,b2=map(float,input("tọa độ B:").split())
+c1,c2=map(float,input("tọa độ C:").split())
+g1=round((a1+b1+c1)/3,2)
+g2=round((a2+b2+c2)/3,2)
+print("tọa đọ trong tâm tam giác:",g1,g2)

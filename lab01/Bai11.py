954d22e5ac346062f45cf1355a13a7b453186175
Linh0001 <ptlinh.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 09:18:09 +0700

    thuc hanh lab02 da hoan thanh Bai01->Bai11

--- lab01/Bai11.py
+++ lab01/Bai11.py
@@ -1,5 +1,4 @@
 n=int(input('Nhập số lần tung xúc xắc: '))
-p=0
-for i in range(1,n+1):
-   p+=1/216
-print("Xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 : %0.2f"%p)
+#Xác suất tung n lần 1 con xúc xắc có ít nhất 1 lần ra 6
+p=1-(5/6)**n
+print("Xác suất khi tung n lần 3 xúc sắc có ít nhất 1 lần cả 3 ra 6 : ",round(p**n,2))
---
+++ lab02/Bai06.py
@@ -0,0 +1,17 @@
+x=int(input('Nhập số nguyên có 3 chữ số: '))
+m={0:'linh',1:'một',2:'hai',3:'ba',4:'bốn',5:'năm',6:'sáu',7:'bảy',8:'tám',9:'chín'}
+a=x//100
+b=(x-a*100)//10
+c=(x-a*100)%10
+if (b==0)&(c==0):
+    print('Số {} đọc là {} trăm'.format(x,m.get(a)))
+elif (b==0)&(c!=0):
+    print('Số {} đọc là {} trăm linh {}'.format(x,m.get(a),m.get(c)))
+elif (b!=0)&(c==0)&(b!=1):
+    print('Số {} đọc là {} trăm {} mươi'.format(x,m.get(a),m.get(b)))
+elif (b==1):
+    print('Số {} đọc là {} trăm mười'.format(x,m.get(a)))
+else:
+    print('Số {} đọc là {} trăm {} mươi {}'.format(x,m.get(a),m.get(b),m.get(c)))
+
+

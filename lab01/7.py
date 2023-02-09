e46612be2e66f3536e2299ef9778df697e194a97
NBDanh76 <nbdanh.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 23:53:55 +0700

    thuc hanh lab01 da hoan thanh bai01/02/03/04/05/06/07/08/09/10

---
+++ lab01/7.py
@@ -0,0 +1,8 @@
+# Phương trình bậc 2 aX^2+bX+c
+a=int(input("Nhập a:"))
+b=int(input("Nhập b:"))
+c=int(input("Nhập c:"))
+print(a,"x^2 +",b,"x + ",c)
+# Tìm đỉnh của phương trình bậc 2
+tim_dinh=-b/2*a
+print("Đỉnh của phương trình bậc 2 là:",round(tim_dinh,2))

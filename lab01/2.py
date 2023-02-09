652062b7ecdf5695677f546795b9f17d67f81c37
unknown <nhnhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-09 21:38:21 +0700

    them cac bai tu 1 den 7 va bai 10

---
+++ lab01/2.py
@@ -0,0 +1,8 @@
+d=int(input("Nhập ngày: "))
+h=int(input("Nhập giờ: "))
+m=int(input("Nhập phút: "))
+s=int(input("Nhập giây: "))
+print("{}ngày,{}giờ,{}phút,{}giây".format(d,h,m,s),"=",round(d+(h/24)+(m/1440)+(s/86400),2),"ngày")
+print("{}ngày,{}giờ,{}phút,{}giây".format(d,h,m,s),"=",round(d*24+h+(m/60)+(s/3600),2),"giờ")
+print("{}ngày,{}giờ,{}phút,{}giây".format(d,h,m,s),"=",round(d*1440+(h*60)+(m)+(s/60),2),"phút")
+print("{}ngày,{}giờ,{}phút,{}giây".format(d,h,m,s),"=",round(s+(m*24)+(h*1440)+(d*86400),2),"giây")
---
+++ lab01/6.py
@@ -0,0 +1,3 @@
+a=220*2.7
+b=int(input("Nhập giây: "))
+print("số tiền phải trả:",((a*b)/3600000)*7000,"vnđ")

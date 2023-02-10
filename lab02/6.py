43f98b818a2289c5a2ed0aebb9aca3fbb06e8244
tuongvi811 <hntvi.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-10 18:36:14 +0700

    first commit

---
+++ lab02/6.py
@@ -0,0 +1,5 @@
+n=input("Nhập chữ số hàng trăm: ")
+m=input("nhập chữ số hàng chục: ")
+o=input("Nhập chũ so")
+num={0:'Không',1:'Một',2:'Hai',3:'Ba',4:'Bốn',5:'Năm',6:'Sáu',7:'Bảy',8:'Tám',9:'Chín'}
+print(num[n],num[n],num[n])

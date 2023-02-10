96f56f9dc945a0a4a5d43eb6b8a13d5a13c1c29d
Khang <nkhang.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-10 09:05:13 +0700

    sua lai bai 6, them bai 11 va 12

--- lab01/bai11.py
+++ lab01/bai11.py
@@ -1,5 +1,9 @@
 #chương trình tính xác xuất tung n lần 3 xúc xắc
-for i in range(1,1001):
-    if i%6!=0 or i%5!=0 :
-        i+=1
-print(i)
+# C=n!/(k!*(n-k)!)
+#xác suất không tung ra cả 3 ra 6 lần nào:
+n=int(input("nhập số lần tung"))
+a=6*6*6#là tập nghiệm
+# xác suất ít nhất 1 lần ra cả 3 xúc xắc là 6:
+b=1
+xac_suat=b/a
+print("xác suất tung n lần 3 xúc xắc là : ",round(xac_suat**n,3))
--- lab01/bai6.py
+++ lab01/bai6.py
@@ -1,6 +1,7 @@
+s=int(input("mời bạn nhập số giây"))
+doi_don_vi=s/60/60
 hieu_dien_the=220
-cuong_do_dong_dien=2,7
+cuong_do_dong_dien=2.7
 cong_suat=hieu_dien_the*cuong_do_dong_dien
-doi_don_vi=cong_suat/1000
-tien_dien=doi_don_vi*7000
-print("tiền điện là: ",tien_dien)
+tien_dien=cong_suat*7000*doi_don_vi
+print("tiền điện là: ",tien_dien/1000)

8455e12ebb08f92f1f7981867d3bffdf87a4da91
may0004 <nthmay.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-10 21:08:11 +0700

    lab01

---
+++ lab01/12.py
@@ -0,0 +1,8 @@
+
+import math
+a=float(input('nhập vào a: '))
+s=-math.pow(a,4)/math.log(4,5)+math.pow(a,4)*math.log(4,5)
+if a>0:
+    print('quãng đường là: ',abs(round(s,2)))
+elif a<0:
+    print("quãng đường là: ",round(s,2))
---
+++ lab01/4.py
@@ -0,0 +1,6 @@
+
+
+import math
+x=int(input("mời nhập x: "))
+giai=(-x+ math.sqrt(math.pow(x,2)+4))/math.pow(math.pow(x,4)+1,1/7)
+print(round(giai,2))

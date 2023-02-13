c3d4c14c01c85228147d356fa0ab744ec90d3deb
PhamThiThuTrang0030-16A1KHDL <119997887+PhamThiThuTrang0030-16A1KHDL@users.noreply.github.com>
2023-02-13 14:33:54 +0700

    Add files via upload

---
+++ Bai09.py
@@ -0,0 +1,6 @@
+x = int(input('Nhập x: '))
+y = int(input('Nhập y: '))
+z = int(input('Nhập z: '))
+print("Tọa độ của điểm đối xứng với nó qua oxy là ({},{},{})".format(x,y,-z))
+print("Tọa độ của điểm đối xứng với nó qua oxz là ({},{},{})".format(x,-y,z))
+print("Tọa độ của điểm đối xứng với nó qua oyz là ({},{},{})".format(-x,y,z))

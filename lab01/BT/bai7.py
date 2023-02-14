5eacc688350c4726d0aeacc38ef670a36ee44b0d
KDHuong0054 <117256453+KDHuong@users.noreply.github.com>
2023-02-14 23:29:27 +0700

    Delete bai7.py

--- lab01/BT/bai7.py
+++
@@ -1,6 +0,0 @@
-print("pt:ax^2+bx+c")
-a=int(input('nhập a: '))
-b=int(input('nhập b: '))
-c=int(input('nhập c: '))
-t=round(-b/(2*a),2)
-print("đỉnh của phương trình là:({},{}) ".format(t,round(a*t*t+b*t+c,2)))

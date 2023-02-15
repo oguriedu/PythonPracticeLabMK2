0c58bbad482f3b2b812f031b6321ded7c2841341
Hoàng Sỹ Việt <hsviet.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-15 17:47:18 +0700

    hoan thanh bt lab01

--- lab01/bai10.py
+++ lab01/bai10.py
@@ -1,4 +1,4 @@
 import math
 x= int(input(" nhập giá trị của x (giá trị nguyên nhoé) :"))
 F = math.log(x,4)+math.log(2,x)
-print(" Giáa trị của  F(x) = : %0.2f"%F)
+print(" Giá trị của  F(x) = : %0.2f"%F)

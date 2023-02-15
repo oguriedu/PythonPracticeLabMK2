7134fd0c566a6560073e3afcfd9e63130da0acee
lethilinhphuong0010 <ltlphuong.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-15 22:17:24 +0700

    hoan thanh lab3

---
+++ lab03/baitaplab3/bai5.py
@@ -0,0 +1,4 @@
+a=int(input('Nhap chieu dai cua dau *: '))
+b=int(input('Nhap chieu rong cua dau *: '))
+for i in range(1,a+1):
+    print('*'*b)
---
+++ lab03/baitaplab3/bai6.py
@@ -0,0 +1,6 @@
+n=int(input("nhập n: "))
+m=0
+for i in range(n):
+    i+=1
+    m+=i*i*i
+print("tổng bậc 3 của ",n," số nguyên đầu tiên là",m)

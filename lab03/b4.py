76dc56dd75b57cf288ca5934fddd9bea42b44e80
vutrongphuc0080 <117803714+vutrongphuc0080@users.noreply.github.com>
2023-02-15 19:47:38 +0700

    thuc hanh lab03 da hoan thanh bai1/bai2/bai3/bai4/bai5/bai6

---
+++ lab03/b4.py
@@ -0,0 +1,31 @@
+def so_nguyen_to(n):
+    flag=1
+    if n<2:
+        flag=0
+        return flag
+    for i in range(2,n):
+        if n%i==0:
+            flag=0
+            break
+    return flag
+            
+            
+#kiem tra
+n=int(input("mời nhập n: "))
+check=so_nguyen_to(n)
+if check==1:
+    print(n,'là số nguyên tố')
+    for j in range(2,n):
+        n-=1
+        j=n
+        check2=so_nguyen_to(j)
+        if check2==1:
+            print("các số nguyên tố bé hơn hoặc bằng là: ",j)
+else:
+    print(n,"không là số nguyên tố")
+    for j in range(2,n):
+        n-=1
+        j=n
+        check2=so_nguyen_to(j)
+        if check2==1:
+            print("các số nguyên tố bé hơn hoặc bằng","là",j)

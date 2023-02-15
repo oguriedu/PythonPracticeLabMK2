476ff0fb3ac9a32636fe516a99972f572d32c9e3
Khoa hoc Dư lieu 16A2 <ldanh.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-15 18:59:48 +0700

    thuc hanh lab03 da hoan thanh bai01/bai02/bai03/bai04/bai05/bai06

---
+++ lab03/bai03.py
@@ -0,0 +1,31 @@
+a= int(input("nhap so "))
+h=a
+m=a
+if (a == 1 ) :
+    print("không phải là số nguyên tố")
+for i in range(2,a+1):
+     if(a==i):
+         print(a,"là số nguyên tố")
+         break
+     if (a%i==0):
+         print(a,"không phải là số nguyên tố")
+         while True:
+            h-=1
+            m+=1
+            for i in range(2,h+1):
+                if(h==i):
+                    print(h,"là số nguyên tố gần",a,'nhất')
+                    h='x'
+                    break
+                if (h%i==0):
+                    break
+            for i in range(2,m+1):
+                if(m==i):
+                    print(m,"là số nguyên tố gần",a,'nhất')
+                    m='x'
+                    break
+                if (m%i==0):
+                    break
+            if ((h=='x')or (m=='x')):
+                break
+         break
---
+++ lab03/bài04.py
@@ -0,0 +1,8 @@
+a= int(input("nhập số "))
+for k in range(2,a+1):
+    for i in range(2,k+1):
+        if(k==i):
+            print(k,"là số nguyên tố")
+            break
+        if (k%i==0):
+            break

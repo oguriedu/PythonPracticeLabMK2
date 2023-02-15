e00c3152e28a24f5fa2482f0e1272c4c2e7518a3
Khang <nkhang.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-15 12:06:58 +0700

    hoan thanh lab 03

---
+++ lab03/baitap/bai2.py
@@ -0,0 +1,21 @@
+n=int(input("mời nhập vào n: "))
+a=0
+for i in range(n+1):
+    n-=1
+    a*=0
+    for j in range(0,n-1):
+        j+=1
+        if n%j==0:
+            a+=j
+    if a==n :
+        print(a)   
+    if n==1:
+        break
+        
+    
+
+
+
+
+
+

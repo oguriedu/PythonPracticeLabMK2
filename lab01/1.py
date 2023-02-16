d9252a6b107256aa219e719df6afbc15295d7e4a
may0004 <nthmay.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-16 19:56:08 +0700

    Hoan thanh lab01

---
+++ lab01/1.py
@@ -0,0 +1,11 @@
+#Viết chương trình nhập vào thông tin của một sinh viên
+ma_sv = input("Nhập mã sinh viên: ")
+ten = input("nhập tên sinh viên: ")
+que_quan = input("nhập quê quán: ")
+nam_sinh = input("nhập năm sinh: ")
+diem_tb_cac_nam_hoc = input("điểm trung bình các năm học: ")
+print("mã số sinh viên là: ",ma_sv)
+print("tên sinh viên là: ",ten)
+print("quê quán sinh viên: ",que_quan)
+print("năm sinh của sinh viên là: ",nam_sinh)
+print("điểm trung bình các năm học: ",diem_tb_cac_nam_hoc)
---
+++ lab01/4.py
@@ -0,0 +1,4 @@
+import math
+x=int(input('nhập giá trị x'))
+f=(-x+ math.sqrt(math.pow(x,2)+4))/math.pow(math.pow(x,4)+1,1/7)
+print('giá trị của f là',f)

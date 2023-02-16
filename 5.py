f2b19ffde8621794708bb1cd51e80c21740ae861
dohien172004 <118988397+dohien172004@users.noreply.github.com>
2023-02-16 11:23:42 +0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,9 @@
+# ve hinh chu nhat
+ 
+hang = int(input("nhập chiều cao: "))
+cot = int(input("nhập chiều  ngang: "))
+ 
+for i in range(1, hang+1):
+    for j in range(1, cot+1):
+        print('*', end='')
+    print()

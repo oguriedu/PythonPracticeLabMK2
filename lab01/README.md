2c6e1ec533f98d3632eff36e071b83e062d3e068
Long <nblong.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-16 18:50:11 +0700

    thuc hanh lab03 da hoan thanh bai1/bai2/bai03/bai4/bai5/bai6

--- lab01/README.md
+++ lab01/README.md
@@ -1,2 +1,6 @@
 - Bài tập luyện tập lab 1
+<<<<<<< HEAD
 DHKL16A1HN-NguyenBaLong-221746000
+=======
+bài tập cơ bản
+>>>>>>> de05b864ebbf7985d84725ce08bcc86309f2db2d
---
+++ lab03/bai1.py
@@ -0,0 +1,8 @@
+num=int(input("nhap so n : "))
+total=1
+a=1
+for i in range(0,num):
+    a*=(2*(i+1))/(2*i+3)
+    total+=a
+    i+=1
+print("ket qua la:",round(total,3))

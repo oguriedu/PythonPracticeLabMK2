6c70ac2da1bed19b8fdfd9f537336eca3410209d
danghuutam16A20022 <dhtam.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-16 19:11:25 +0700

    thuc hanh part1 lab03 hoan thanh

---
+++ lab03/bai04.py
@@ -0,0 +1,9 @@
+n=int(input('Nhập n = '))
+print('Số nguyên tố nhỏ hơn hoặc bằng n là:')
+for i in range(1,n+1):
+    so_uoc=0
+    for j in range(1,i+1):
+        if i%j==0:
+            so_uoc+=1
+    if so_uoc==2:
+        print(i,end=' ')

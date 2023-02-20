499ccb1b9a9febdac45f31e74f853c7375563897
Khang <nkhang.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-20 18:15:38 +0700

    sua lai bai 3

--- lab03/baitap/bai3.py
+++ lab03/baitap/bai3.py
@@ -1,28 +1,34 @@
-def so_nguyen_to(n):
-    flag=1
-    if n<2:
-        flag=0
-        return flag
-    for i in range(2,n):
-        if n%i==0:
-            flag=0
-            break
-    return flag
-            
-            
-#kiem tra
-n=int(input("mời nhập n: "))
-check=so_nguyen_to(n)
-if check==1:
-    print(n,'là số nguyên tố')
+n=int(input("nhap so duong n: "))
+if n<2:
+    print(f"{n} khong phai so nguyen to")
+if n == 1 or n == 0  :
+    print('so nguyen to gan nhat la 2')
+flag=True
+for i in range (2,n,1):
+    if n%i==0:
+        flag=False
+        print(f"{n} khong phai so nguyen to")
+        break
 else:
-    print(n,"không là số nguyên tố")
-    for j in range(2,n):
-        n-=1
-        j=n
-        check2=so_nguyen_to(j)
-        if check2==1:
-            print(j)
+    print(f"{n} la so nguyen to")
+
+if flag==False:
+    for i in range(n-1,2,-1):
+        for j in range(2,i,1):
+            if i % j ==0:
+                break
+        else:
+            print(f"{i} la so nguyen to gan nhat ben trai")
             break
-        
-            
+else:
+    print(f"khong co so nguyen to nao ben trai {n}")
+for i in range(n+1,10000000,1):
+    for j in range(2,i,1):
+        if i%j==0:
+            break
+    else:
+        print(f"{i} la so nguyen to gan nhat ben phai")
+        break
+else:
+    print(f" khong co so nguyen to ben phai{n}")
+

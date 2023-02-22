413bb5782d4e49c56fcf5a9049e97c8e8041d433
vutrongphuc0080 <117803714+vutrongphuc0080@users.noreply.github.com>
2023-02-22 18:46:44 +0700

    thuc hanh lab04 da hoan thanh bai1/bai2/bai3/bai4/bai5/bai7/bai8/bai9/bai10

---
+++ lab04/b1.py
@@ -0,0 +1,15 @@
+n = int(input("Nhập n: "))
+while n<=0:
+    n = int(input("Nhập n > 0: "))
+s4=0
+s5=0
+s6=0
+a=1
+while a!=(n+1):
+    s4+=a**2
+    s5+=(2*a+1)**3
+    s6+=(2*n)**4
+    a+=1
+print('S4= ',s4)
+print("S5=",s5)
+print("S6=",s6)
---
+++ lab04/b5.py
@@ -0,0 +1,4 @@
+#nhập số bất kì khi đến 0 thì stop
+x=0
+while x>=0:
+    x=int(input("nhập cho đến số âm thì dừng : "))

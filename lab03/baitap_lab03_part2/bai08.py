960d1a983d23ec519b0d9c269e6efa1bb42cfb8c
lqlinhkhdl <lqlinh.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-22 16:19:29 +0700

    hoan thanh bai tap lab03 part2

---
+++ lab03/baitap_lab03_part2/bai08.py
@@ -0,0 +1,10 @@
+n=int(input('Nhập n = '))
+while n<=0:
+    print('Mời nhập lại n>0 !')
+    n=int(input('Nhập n = '))
+s1=(n*(n+1))/2
+s2=(n+1)*2
+s3=n*(n+1)
+print('S1 = ',s1)
+print('S2 = ',s2)
+print('S2 = ',s3)

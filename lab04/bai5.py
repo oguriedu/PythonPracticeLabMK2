cb1875d3bd9232d9c3d7dc81aac1dc9c392e92f2
Hoa-0052 <117371997+NguyenThiThanhHoa0052-16A1@users.noreply.github.com>
2023-02-23 21:50:36 +0700

    Add files via upload
    Đã hoàn thành lab04

---
+++ lab04/bai5.py
@@ -0,0 +1,3 @@
+h=1
+while h >= 0 :
+    h=int(input("nhập số bất kỳ "))
---
+++ lab04/bai9.py
@@ -0,0 +1,8 @@
+h=1
+m=0
+while h==1 :
+    t=int(input("nhập số: "))
+    h=int(input("bạn có muốn tiếp tục nhập không 1:có ,2:không "))
+    m+=t
+print("tổng của các phần tử vừa nhập là:",m)
+ break

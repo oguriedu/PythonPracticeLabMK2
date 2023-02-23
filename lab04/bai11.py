5ec1bf0ba6bde1a7c38a36ae30bfb813aca21070
khdl16a1 tungson <ntson.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-23 21:52:57 +0700

    update bai 11

--- lab04/bai11.py
+++ lab04/bai11.py
@@ -12,19 +12,12 @@ while True:
         print("4. Nước lọc")
         print("5. Nước dừa")
         order = int(input("Chọn thức uống theo số trên menu: "))
-        if order == 1:
+        try:
             l.append(drink[order-1])
-        if order == 2:
-            l.append(drink[order-1])
-        if order == 3:
-            l.append(drink[order-1])
-        if order == 4:
-            l.append(drink[order-1])
-        if order == 5:
-            l.append(drink[order-1])
-        else:
+            n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4:xóa order cũ "))
+        except:
             print("Số bạn chọn không hợp lệ. Vui lòng thử lại.")
-        n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4:xóa order cũ "))
+            n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4:xóa order cũ "))
     elif n==3:
         break
     elif n==2:
@@ -42,11 +35,14 @@ while True:
             n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4: xóa order cũ "))
         except:
             print("thứ tự không hợp lệ")
-        n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4: xóa order cũ "))
+            n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4: xóa order cũ "))
     else:
         print("lựa chọn của bạn không hợp lệ")
         n=int(input("bạn muốn làm gì tiếp: 1:chọn đồ tiếp ,2:xem lại order, 3:dừng lại, 4: xóa order cũ "))
 os.system("cls")
-for i in l:
-    print(f"bạn đã chọn {i}")
+if len(l)>=1:
+    for i in l:
+        print(f"bạn đã chọn {i}")
+else:
+    print("bạn không chọn gì")
     

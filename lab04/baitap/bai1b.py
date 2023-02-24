ef09ff4e3db7a2ec9ef96debf632827a20a85912
vuduchieu0002 <117256523+vuduchieu0002@users.noreply.github.com>
2023-02-24 18:57:27 +0700

    hoan thanh lab04

---
+++ lab04/baitap/bai1b.py
@@ -0,0 +1,9 @@
+tong=0
+i=1
+n=int(input("Nhap gia tri n : "))
+while n<=0:
+    print("Nhap lai gia tri : ")
+    n=int(input())
+for i in range(1,n+1):
+    tong+=(2*i+1)**3
+print("Tong s4 la : ",tong)
---
+++ lab04/baitap/bai2c.py
@@ -0,0 +1,10 @@
+import math
+tong=0
+i=1
+n=int(input("Nhap gia tri n : "))
+while n<=0:
+    print("Nhap lai gia tri : ")
+    n=int(input())
+for i in range(1,n+1):
+    tong+=1/math.sqrt(i)
+print("Tong s la : ",round(tong,2))

ddd35248382479615d5637f18764865e0cd74c18
NamPham009 <117257953+namphamth@users.noreply.github.com>
2023-02-25 23:05:47 +0700

    thực hành lab04 hoàn thành trừ bài 3,10

--- lab04/lab04_baitap/b3.py
+++
@@ -1,8 +0,0 @@
-mau_so=int(1)
-while  mau_so >0:
-    tu_so=int(input("Nhập tử số:"))
-    mau_so=int(input("Nhập mẫu số:"))
-    if mau_so==0:
-        mau_so=int(input("Nhập lại mẫu số khác 0:"))
-    print('phân số là :')
-    print(tu_so,"/",mau_so)

f555527498cb8f9e3f314e784084fd6c5cd810c6
pvthang29012004 <119024786+pvthang29012004@users.noreply.github.com>
2023-02-25 18:08:45 +0700

    hoàn thành bài tập lab04

---
+++ lab04/bài tập lab04/bài1.py
@@ -0,0 +1,21 @@
+#Phần a
+n=int(input('nhập số: '))
+i=0; s=0
+while i<=n:
+    s=s+i**2
+    i+=1
+print(s)
+#Phần b
+n=int(input('nhập số: '))
+i=0; s=0
+while i<=n:
+    s=s+(2*i+1)**3
+    i+=1
+print(s)
+#Phần c
+n=int(input('nhập số: '))
+i=0; s=0
+while i<=n:
+    s=s+(2*i)**4
+    i+=1
+print(s)
---
+++ lab04/bài tập lab04/bài3.py
@@ -0,0 +1,17 @@
+x = float(input("Nhập giá trị của x: "))
+
+cos_x = 1 
+
+m = 1 
+
+n = 2
+
+while abs(m) > 1e-4:
+
+    m = -m * x**2 / (n * (n - 1)) 
+
+    cos_x += m 
+
+    n += 2 
+
+print("Giá trị xấp sỉ của cos", x, "là", cos_x)

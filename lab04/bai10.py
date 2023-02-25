9c7ab63f7849296bfe1dff6d183a67e379db1c98
cafedevn <nqninh.dhkl16a1hn@uneti.edu.vn>
2023-02-25 21:03:30 +0700

    thuc hanh lab04 da hoan thanh bai01/bai02/bai03/bai04/bai05/bai06/bai07/bai08/bai09/bai10

---
+++ lab04/bai10.py
@@ -0,0 +1,16 @@
+nhap=float(input('moi nhap so thap phan: '))
+so=[]
+dem=["khong","mot","hai","ba","bon","nam","sau","bay","tam","chin"]
+for i in str(nhap):
+    if i==".":
+        continue
+    else:
+        so.append(i)
+chuoi=" "
+for num in so:
+    for j in range(0,len(dem)):
+        if int(num)==j:
+            chuoi+=dem[j]
+            chuoi+=" "
+print(nhap)
+print(chuoi )

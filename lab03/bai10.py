311bddf3082da8bd16d7915aebf59ad133cf021d
nguyenngocbacdhkl16a2hn0045 <117519519+nguyenngocbacdhkl16a2hn0045@users.noreply.github.com>
2023-02-25 13:11:04 +0700

    lab3 part2

---
+++ lab03/bai10.py
@@ -0,0 +1,10 @@
+n = int(input("Nhập n = "))
+i = 2
+print("thừa số nguyên tố của",n,"là: ")
+while n !=1:
+
+    if n % i == 0:
+        print(i,end = " ")
+        n = n // i
+    else:
+        i = i + 1
---
+++ lab03/bai7.py
@@ -0,0 +1,6 @@
+n=int(input("nhập n: "))
+s=0
+for i in range(1,n+1):
+    s+=1/i
+print("tổng là:",s)
+

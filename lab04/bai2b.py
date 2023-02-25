4b5a317f12e988d4b24f05ac86abcac71a6eda67
Nguyen Duc Tam 0102 <ndtam.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-25 16:56:21 +0700

    thuc hanh lab04 da hoan thanh bai01/bai02/bai03/bai04/bai06/bai07/bai08/bai09/bai10/bai11

---
+++ lab04/bai2b.py
@@ -0,0 +1,11 @@
+n=int(input("moi nhap n: "))
+tong=0
+while n<=0:
+    n=int(input("moi nhap lai n: "))
+    if n>=0:
+        break
+for i in range(1,n+1):
+    tong+=(1/i)*(1/(i+1))
+    if i==n:
+        break
+print(tong)

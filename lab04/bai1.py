e26b5f7551f4141dca6c7a1337918c4c2e00d5ff
PThanh0049 <npthanh.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-25 15:11:43 +0700

    thuc hanh lab04 hoan thanh

--- lab04/bai1.py
+++ lab04/bai1.py
@@ -1,11 +1,28 @@
+n = int(input("Nhập n: "))
+while n <= 0:
+    n = int(input("Nhập lại n (n > 0): "))
 
-n=int(input("moi nhap n: "))
-tong=0
-while True:
-    if n<=0:
-        n=int(input("moi nhap lai n: "))
-    for i in range(0,n+1):
-        tong+=i**2
-    if i==n:
-        break
-print(tong)
+# Tính tổng S4
+S4 = 0
+i = 1
+while i <= n:
+    S4 += i ** 2
+    i += 1
+
+# Tính tổng S5
+S5 = 0
+i = 1
+while i <= (2 * n + 1):
+    S5 += i ** 3
+    i += 2
+
+# Tính tổng S6
+S6 = 0
+i = 2
+while i <= (2 * n):
+    S6 += i ** 4
+    i += 2
+
+print("Tổng S4 =", S4)
+print("Tổng S5 =", S5)
+print("Tổng S6 =", S6)
---
+++ lab04/bai8.py
@@ -0,0 +1,8 @@
+# Nhập ký tự từ bàn phím
+char = input("Nhập một ký tự: ")
+
+# Tìm giá trị ASCII của ký tự đó
+ascii_value = ord(char)
+
+# In ra giá trị ASCII
+print("Giá trị ASCII của ký tự", char, "là", ascii_value)

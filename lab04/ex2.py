429026c79b598a2093dffff162e1002e5cf3f43d
Chuong10 <hqvinh.dhkl16a2hn@sv.uneti.edu.vn>
2023-02-25 18:33:53 +0700

    Hoan thanh lab04

--- lab04/ex2.py
+++ lab04/ex2.py
@@ -1,5 +1,6 @@
 import math
 n=int(input("Nhập số:"))
+#ys A
 ex =0
 for a in range(1,n+1):
     if a%2==0:
@@ -7,6 +8,7 @@ for a in range(1,n+1):
     else:
         ex +=1/a
 print(ex)
+# ys C
 sum = 0
 for i in range(2,n+1):
     sum += 1/math.sqrt(i)

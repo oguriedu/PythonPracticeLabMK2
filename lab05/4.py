5b870ffcb5e05a004f8c52a0ffea99a0b2bda04f
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-27 23:20:23 +0700

    Bài tập lab05

---
+++ lab05/4.py
@@ -0,0 +1,20 @@
+Str1=input('Nhập Str1: ')
+Str2=input('Nhập Str2: ')
+if len(Str1)==len(Str2):
+    for i,j in zip(Str1,Str2):
+        print(i+j,end='')
+elif len(Str1)>len(Str2):
+    for i,j in zip(Str1,Str2):
+        print(i+j,end='')
+    for i in Str1:
+        if Str1.index(i)<=(len(Str2)-1):
+            continue
+        print(i,end='')
+else:
+    for i,j in zip(Str1,Str2):
+        print(i+j,end='')
+    for i in Str2:
+        if Str2.index(i)<=(len(Str1)-1):
+            continue
+        print(i,end='')
+    

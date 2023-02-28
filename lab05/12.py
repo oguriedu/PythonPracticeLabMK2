157a5b4850d61a663a66d2a27cf7906f8ff0ef47
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-02-28 10:59:03 +0700

    Fix lỗi bài 12

--- lab05/12.py
+++ lab05/12.py
@@ -3,16 +3,26 @@ Str2 = input()
 list_Str1 = []
 list_Str2 = []
 for i in range(1,len(Str1)):
-    so_dau = int(Str1[:i])
-    so_cuoi = int(Str1[i:])
+    so_dau = Str1[:i]
+    so_cuoi = Str1[i:]
     list_Str1.append([so_dau,so_cuoi]) 
 for i in range(1,len(Str2)):
-    so_dau = int(Str2[:i])
-    so_cuoi = int(Str2[i:])
+    so_dau = Str2[:i]
+    so_cuoi = Str2[i:]
     list_Str2.append([so_dau,so_cuoi])
-for i,j in zip(list_Str1,list_Str2):
-    if i[0]+i[1]==j[0]+j[1]:
-        print(f"{i[0]}+{i[1]}={j[0]}+{j[1]}")
-        break
+for i in list_Str1:
+    for j in list_Str2:
+      if int(i[0])+int(i[1])==int(j[0])+int(j[1]):
+            print(f"{i[0]}+{i[1]}={j[0]}+{j[1]}")
+            break
+    else:
+        continue
+    break
 else:
     print('Không tồn tại cách đặt')
+# for i,j in zip(list_Str1,list_Str2):
+#     if i[0]+i[1]==j[0]+j[1]:
+#         print(f"{i[0]}+{i[1]}={j[0]}+{j[1]}")
+#         break
+# else:
+#     print('Không tồn tại cách đặt')

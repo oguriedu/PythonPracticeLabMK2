5da9bc5d9053d5050d269903e71ab6ddde380797
Nguyễn Thị Lan Anh <117256673+Lananh0026@users.noreply.github.com>
2023-03-04 22:49:05 +0700

    Update bai9.py

--- lab05/Bt_lab05/bai9.py
+++ lab05/Bt_lab05/bai9.py
@@ -1,15 +1,10 @@
+#Chua ht nốt
 str1 = input("Nhập chuỗi ký tự Str1: ")
 str2 = input("Nhập chuỗi ký tự Str2: ")
-
-# Khởi tạo chuỗi con chung của 2 chuỗi
-common_str = ""
-
-# Tìm ra chuỗi con chung của 2 chuỗi
+bd_str = ""
 for i in range(min(len(str1), len(str2))):
     if str1[i] == str2[i]:
-        common_str += str1[i]
+        bd_str += str1[i]
     else:
         break
- 
-# In ra chuỗi con chung của 2 chuỗi
-print("Chuỗi con chung của Str1 và Str2 là:", common_str)
+print("Chuỗi con chung của Str1 và Str2 là:", bd_str)

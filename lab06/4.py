199e04906d6095fa8c47187418db250d7a671311
Hung0056 <tmhung.dhkl16a1hn@sv.uneti.edu.vn>
2023-03-10 20:56:59 +0700

    Lab06

--- lab06/4.py
+++ lab06/4.py
@@ -14,4 +14,10 @@ for i in lst:
     numbers.insert(0,i)
 lst.sort()
 numbers.extend(lst)
-print(numbers)
+print('Sau khi chèn danh sách [1,2,3] vào vị trí đầu, cuối, thứ 5 ta được danh sách',numbers)
+k=int(input('Nhập vị trí cần xóa:  '))
+lst2=numbers.pop(k)
+print(f"Phần tử thứ {k} là {lst2} đã bị xóa khỏi list")
+print('List sau khi sắp xếp theo thứ tự tăng dần là',sorted(numbers))
+print('List sau khi sắp xếp theo thứ tự giảm dần là',sorted(numbers,reverse=True))
+

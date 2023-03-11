464614dea54b8dc7b22c1caabc568fc7baa0c2c1
khoa hoc du lieu 16a2 <ldthang.dhkl16a2hn@sv.uneti.edu.vn>
2023-03-11 18:54:17 +0700

    tuple chua hoan thanh

---
+++ lab06/6.py
@@ -0,0 +1,19 @@
+import random
+list = []
+
+for i in range(1000):
+  num = random.randint(1,99999)
+  list.append(num)
+
+print(list)
+print(len(list))
+
+list.sort(reverse=False)
+print("Sap xep tang dan khi dung sort:",list)
+
+n = len(list)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if list[j] > list[j+1]:
+            list[j], list[j+1] = list[j+1], list[j]

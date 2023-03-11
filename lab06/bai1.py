f831eb5890f9aa2db6f2f770838cefe83439b400
Hoa-0052 <117371997+NguyenThiThanhHoa0052-16A1@users.noreply.github.com>
2023-03-11 16:12:22 +0700

    Add files via upload
    bài tập lab06

---
+++ lab06/bai1.py
@@ -0,0 +1,16 @@
+a =[2,-4,1,9,-3,6,3,-2,6,8]
+tong=0
+duong=0
+am = 0
+for i in a:
+    tong+=i
+    if i>0:
+       duong+=i
+for j in a:
+    if j<0:
+        am+=j
+        break
+lon_nhat=0
+print(tong)
+print(duong)
+print(am)

f8b3aec9f58213581530f46d40a3679444cd9a6e
Hoàng Sỹ Việt <hsviet.dhkl16a1hn@sv.uneti.edu.vn>
2023-03-12 15:20:20 +0700

    hoàn thành lab06

--- lab01/bai9.py
+++
@@ -1,5 +0,0 @@
-a=input("nhập tọa độ điểm trên mặt phẳng oxyz (cách nhau bởi dấu ;): ")
-t=a.split(";")
-print("tọa độ điểm đối xứng qua oxy là ({},{},{})".format(int(t[0]),int(t[1]),-int(t[2])))
-print("tọa độ điểm đối xứng qua oxz là ({},{},{})".format(int(t[0]),-int(t[1]),int(t[2])))
-print("Tọa độ điểm đối xứng qua oyz là ({},{},{})".format(int(t[0]),int(t[1]),int(t[2])))
---
+++ lab06/bai3.py
@@ -0,0 +1,21 @@
+
+numbers=[]
+while True: 
+    number = int(input("Nhập vào một số nguyên (nhập 0 để kết thúc nhập): ")) 
+    if number == 0: 
+        break 
+    numbers.append(number)
+numbers2=[]
+numbers1=[]
+for i in numbers:
+    if i>0:
+        numbers1.append(i)
+    else:
+        numbers2.append(i)
+numbers1.extend(numbers2)
+print(f"Chuyển phần tử dương lên đầu ta có danh sách sau {numbers1}")
+m=int(input('Nhập m: '))
+numbers.insert(4,m)
+numbers.append(m)
+numbers.insert(0,m)
+print(f"Chèn m vào đầu, cuối, ví trị thứ 5 ta được danh sách {numbers}")

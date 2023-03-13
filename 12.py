525ccc493a6624a00646765f5d2d6ebbf8119e95
le thi phuong linh 16a1/0057.uneti.hn <ltplinh.dhkl16a1hn@sv.uneti.edu.vn>
2023-03-13 07:51:19 +0700

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,41 @@
+import os
+clear='cls'
+tk='LeThiPhuongLinh'
+stk='22174600057'
+money=0
+history=[]
+
+while True:
+    print(f"""                                              
+    -------------------NGÂN HÀNG xXxBANK-------------------------                      
+      Tên tài khoản: {tk}                                         
+      Số tài khoản: {stk}                                     
+      Số dư tài khoản: {money}
+    [1] Rút tiền 
+    [2] Nạp tiền 
+    [3] Kiểm tra lịch sử giao dịch                                    
+    [0] Thoát                                                     
+    """)
+    chon=input()
+    if chon=='1':
+        Withdraw=int(input('W '))
+        money-=Withdraw
+        mess = f"Rút: {Withdraw} Số dư: {money}"
+        history.append(mess)
+        os.system(clear)
+    elif chon=='2':
+        Deposit=int(input('D '))
+        money+=Deposit
+        mess = f"Nạp: {Deposit} Số dư: {money}"
+        history.append(mess)
+        os.system(clear)
+    elif chon=='3':
+        os.system(clear)
+        for i in history:
+            print(i)
+        input('Nhấn bất kỳ để tiếp tục')
+        os.system(clear)
+    elif chon=='4':
+        print(history)
+    elif chon=='0':
+        break
